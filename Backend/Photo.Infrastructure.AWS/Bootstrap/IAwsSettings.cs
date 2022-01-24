21887310b264f1e3e48fa19bec6ac3f4dade8099
dcarbajal <daniel.carbajal@pucp.edu.pe>
2022-01-23 23:36:40 -0500

    Adding S3 support

---
+++ Backend/Photo.Infrastructure.AWS/Bootstrap/IAwsSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.Infrastructure.AWS.Bootstrap
+{
+    public interface IAwsSettings
+    {
+        public string Key { get; }
+        public string Secret { get; }
+        public string BucketName { get; }
+        public string AWSRegion { get; }
+        public string? AwsUrl { get; }
+    }
+}
---
+++ Backend/Photo.Infrastructure.AWS/S3FileRepository.cs
@@ -0,0 +1,66 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Photo.Domain.External.File;
+using Photo.Domain.External.File.Common;
+using Photo.Infrastructure.AWS.Bootstrap;
+
+namespace Photo.Infrastructure.AWS
+{
+    public class S3FileRepository : IFileRepository
+    {
+        private readonly IAwsSettings _AWSSettings;
+
+        public S3FileRepository(IAwsSettings AWSSettings)
+        {
+            this._AWSSettings = AWSSettings;
+        }
+
+        public async Task<byte[]> GetFileByName(string fileName)
+        {
+            using (var s3Client = this.buildS3Config())
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var response = await s3Client.GetObjectAsync(this._AWSSettings.BucketName, fileName);
+                    response.ResponseStream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public async Task<ImageToSaveResponse> SaveFile(ImageToSaveRequest request)
+        {
+            using (var s3Client = this.buildS3Config())
+            {
+                await s3Client.PutObjectAsync(new PutObjectRequest
+                {
+                    BucketName = this._AWSSettings.BucketName,
+                    Key = request.FileName,
+                    CannedACL = S3CannedACL.PublicRead
+                });
+            }
+            return new ImageToSaveResponse
+            {
+                URL = buildFileUrl(request.FileName)
+            };
+        }
+
+        private AmazonS3Client buildS3Config()
+        {
+            var config = new AmazonS3Config();
+            if (this._AWSSettings.AwsUrl != null)
+            {
+                config.ServiceURL = this._AWSSettings.AwsUrl;
+            }
+            return new AmazonS3Client(this._AWSSettings.Key, this._AWSSettings.Secret, config);
+        }
+
+        private string buildFileUrl(string fileName)
+        {
+            return this._AWSSettings.AwsUrl != null ? 
+                $"{this._AWSSettings.AwsUrl}/{this._AWSSettings.BucketName}/{fileName}" : 
+                $"https://${this._AWSSettings.BucketName}.s3.${this._AWSSettings.AWSRegion}.amazonaws.com/${fileName}";
+        }
+
+    }
+}
